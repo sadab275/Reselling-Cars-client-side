ba5c65147db00167cf75501adb60b82deb66304e
sadab275 <sadabrahman275@gmail.com>
2022-11-28 21:38:02 +0600

    banner edited

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
                 <img src={banner} className="w-1/2 rounded-lg shadow-2xl" alt='' />
                 <div>
                     <h1 className="text-5xl text-success font-bold">Looking to buy an affordable Car?</h1>
-                    <p className="text-text-lime-900 py-6">If you are looking for second hand cars with the minimum price, you are in the right place. </p>
+                    <p className="text-text-lime-900 py-6">If you are looking for second hand cars with the minimum price, you are in the right place.We provide best condition cars with minimum price. </p>
                     <button className="btn btn-info">Get Started</button>
                 </div>
             </div>
